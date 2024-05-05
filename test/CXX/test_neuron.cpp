117c7325da509a423a61dd8cac4628651ed6f5ef
Ken Tanaka Hernández <ktanaka@ictp.it>
2024-05-05 05:32:21 +0200

    Tests

---
+++ test/CXX/test_neuron.cpp
@@ -0,0 +1,41 @@
+#include "gtest/gtest.h"
+#include "neuron.hpp"
+
+TEST(TestNeuron, bool) {
+  neuron myNeuron();
+}
+
+TEST(TestNeuron, int) {
+}
+
+TEST(TestNeuron, double) {
+}
+
+TEST(TestNeuron, std::complex<double>) {
+}
+
+
+TEST(TestLayer, bool) {
+}
+
+TEST(TestLayer, int) {
+}
+
+TEST(TestLayer, double) {
+}
+
+TEST(TestLayer, std::complex<double>) {
+}
+
+
+TEST(TestBrain, bool) {
+}
+
+TEST(TestBrain, int) {
+}
+
+TEST(TestBrain, double) {
+}
+
+TEST(TestBrain, std::complex<double>) {
+}
